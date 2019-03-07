a8f582b8a97765cb7c3178535edc8cf4957b28dd
dknapp16 <47129329+dknapp16@users.noreply.github.com>
2019-03-06 21:31:44 -0600

    Add files via upload

---
+++ Homework9.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Homework9 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = generateIntegers(20);
+        for (int i : integers) {
+            System.out.println("i = " + i);
+            if (i % 15 == 0) { //a number divisible by 3 and 5 is also divisible by 15;
+                System.out.println("Fizzbuzz");
+                System.out.println();
+                i++;
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+                System.out.println();
+                i++;
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+                System.out.println();
+                i++;
+            } else {
+                System.out.println("Number " + i + " is not fizzy.");
+                System.out.println();
+            }
+        }
+    }
+
+
+    public static ArrayList<Integer> generateIntegers(int size) {
+        ArrayList<Integer> xs = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            xs.add(Math.abs(random.nextInt(Integer.MAX_VALUE - 1) + 1));
+        }
+        return xs;
+    }
+}
+
+
+
+
